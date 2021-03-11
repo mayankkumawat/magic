2d2e12e94e01bd1b4d56414dfbce9daf9770eca4
mayank <mayankkumawat015@gmail.com>
2021-03-11 18:15:12 +0530

    code modified

--- checkbox.js
+++ checkbox.js
@@ -11,35 +11,27 @@ $(document).ready(function(){
             expanded = false;
         }
     });
-    $(".parent").click(function(){
-        $(".child").find("input[type='checkbox']").prop("checked",true);
+   
+    // parent check, child check | parent uncheck, child uncheck
+
+    $("#checkboxes").on('change','.parent',function(){
+        $(this).next().children().find("input[type='checkbox']").prop("checked", $(this).children().find("input[type='checkbox']").prop("checked"));
     });
+
+    // single child check, parent will checked automatically
+    
+    $(".child").click(function(){
+        $(this).prev().find("input[type='checkbox']").prop("checked",true);
+    });
+
+    // all childs unselected, parent unselected
 
     $(".child").click(function(){
         if(!($(this).find("input[type='checkbox']").is(':checked'))){
-            $(".parent").find("input[type='checkbox']").prop("checked",false);
-
-        }      
-    });
-
-    $(".child").find("input[type='checkbox']").click(function(){
-            $(".parent").find("input[type='checkbox']").prop("checked",true);
-    });    
+            $(this).prev().find("input[type='checkbox']").prop("checked",false);
     
-    $(".parent1").click(function(){
-        $(".child1").find("input[type='checkbox']").prop("checked",true);
-    });
-
-    $(".child1").click(function(){
-        if(!($(this).find("input[type='checkbox']").is(':checked'))){
-            $(".parent1").find("input[type='checkbox']").prop("checked",false);
-
         }      
     });
-
-    $(".child1").find("input[type='checkbox']").click(function(){
-        $(".parent1").find("input[type='checkbox']").prop("checked",true);
-    });
-
+  
 });
 
